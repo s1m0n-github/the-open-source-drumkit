3d9957d956394dc73b0405034e099d1b6c8fd6d5
s1m0n-github <simon.waldek@gmail.com>
2022-03-17 08:35:40 +0100

    tested Button

--- osd-plugin/Source/PluginEditor.cpp
+++ osd-plugin/Source/PluginEditor.cpp
@@ -13,9 +13,17 @@
 NewProjectAudioProcessorEditor::NewProjectAudioProcessorEditor (NewProjectAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize (400, 300);
+    
+    testDrum.setSliderStyle (juce::Slider::LinearBarVertical);
+    testDrum.setRange (0.0, 127.0, 1.0);
+    testDrum.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0);
+    testDrum.setPopupDisplayEnabled (true, false, this);
+    testDrum.setTextValueSuffix (" Volume");
+    testDrum.setValue(1.0);
+    
+    addAndMakeVisible (&testDrum);
+    addAndMakeVisible (&testButton);
 }
 
 NewProjectAudioProcessorEditor::~NewProjectAudioProcessorEditor()
@@ -30,11 +38,13 @@ void NewProjectAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Test Slider", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void NewProjectAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    testDrum.setBounds (40, 30, 20, getHeight() - 60);
+    testButton.setBounds (150, 100, 100, 100);
 }
--- osd-plugin/Source/PluginEditor.h
+++ osd-plugin/Source/PluginEditor.h
@@ -28,6 +28,9 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     NewProjectAudioProcessor& audioProcessor;
-
+    
+    juce::Slider testDrum;
+    juce::TextButton testButton {"Test"};
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessorEditor)
 };
